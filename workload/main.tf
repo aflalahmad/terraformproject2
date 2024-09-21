7586f88860f1b65fda248c4fd09116d91f3ace97
Aflalahmad <a.aflalahamed@gmail.com>
2024-09-21 11:50:14 +0530

    update

--- workload/main.tf
+++ workload/main.tf
@@ -15,5 +15,66 @@ module "virtualnetwork" {
   location            = var.location
   name                = each.key
   resource_group_name = module.rg.name
-   
+  
+  subnets = {
+    subnet1 = {
+      name = each.value.subnets["subnet1"].name
+      address_prefixes = [each.value.subnets["subnet1"].address_prefixes]
+      zone = each.value.subnets["subnet1"].zone
+    },
+     subnet2 = {
+      name = each.value.subnets["subnet2"].name
+      address_prefixes = [each.value.subnets["subnet2"].address_prefixes]
+      zone = each.value.subnets["subnet2"].zone
+    },
+     subnet3 = {
+      name = each.value.subnets["subnet3"].name
+      address_prefixes =[ each.value.subnets["subnet3"].address_prefixes]
+      zone = each.value.subnets["subnet3"].zone
+    }
+  }
+  depends_on = [ module.rg ]
 }
+
+module "networksecuritygroup" {
+  source  = "Azure/avm-res-network-networksecuritygroup/azurerm"
+  version = "0.2.0"
+  for_each = toset(var.nsg_name)
+  name = each.key
+  location = var.location
+  resource_group_name = module.rg.name
+}
+
+
+module "publicipaddress" {
+  source  = "Azure/avm-res-network-publicipaddress/azurerm"
+  version = "0.1.2"
+  for_each = var.public_ip
+  name = each.key
+  location = var.location
+  resource_group_name = module.rg.name
+  sku = each.value.sku
+  sku_tier = each.value.sku_tier
+  allocation_method = each.value.allocation_method
+
+}
+
+# module "loadbalancer" {
+#   source  = "Azure/avm-res-network-loadbalancer/azurerm"
+#   version = "0.2.2"
+#   for_each = var.lb
+#   name = each.key
+#   location = var.location
+#   resource_group_name = module.rg.name
+#   sku = each.value.sku
+#   sku_tier = each.value.sku_tier
+  
+# #   frontend_ip_configurations = {
+# #     frontend_configuration_1 = {
+# #       name = each.value.frontendip["frontendip_config"].name
+# #       frontend_private_ip_address_version    = each.value.frontendip["frontendip_config"].frontend_private_ip_address_version
+# #       frontend_private_ip_address_allocation = each.value.frontendip["frontendip_config"].frontend_private_ip_address_allocation
+# #       zones = each.value.frontendip["frontendip_config"].zones
+# #     }
+# # }
+# }
--- workload/variable.tf
+++ workload/variable.tf
@@ -14,6 +14,43 @@ variable "virtual_network" {
 type = map(object({
   name = string
   address_space = string
-  
+  subnets = map(object({
+    name = string
+    address_prefixes = string
+    zone = string 
+  }))
 })) 
 }
+
+variable "nsg_name" {
+  type = list(string)
+  
+}
+
+variable "public_ip" {
+  type = map(object({
+    name =string
+    sku = string
+    allocation_method = string
+    sku_tier = string
+  }))
+  
+}
+
+# variable "lb" {
+#   type = map(object({
+#      name = string
+#      sku = string
+#      sku_tier  =string
+#      edge_zone = string
+#      frontendip = map(object({
+#        name = string
+#         frontend_private_ip_address_version = string
+#            frontend_private_ip_address_allocation = string
+#            zones = list(string)
+#      }))
+     
+
+#   }))
+  
+# }
